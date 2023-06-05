1b5bf109cbbce802ae99295a0291530965e18046
ThomasBiscan <134562752+ThomasBiscan@users.noreply.github.com>
2023-06-05 05:05:00 +0000

    added questions and answers

--- list.py
+++ list.py
@@ -1,16 +1,28 @@
-player1_questions = ["Name a fruit.", "Name a country.", "Name a superhero.", "Name a movie."]
+player1_questions = ["If you see pink frosting on a cake, what flavor would you expect it to be? ", "Name A Specific Night Of The Year That You Hope To Be Kissed?", "What Might A Parent Videotape Their Child Doing Around The Christmas Holiday?", "	Name A Profession That’s Only Successful If People Are Laughing At Them?"]
 player1_answers = [
-    ["Apple", "Banana", "Orange", "Strawberry","Mandarin"],
-    ["USA", "Canada", "Japan", "Australia"],
-    ["Superman", "Spiderman", "Batman", "Wonder Woman"],
-    ["Titanic", "Star Wars", "The Avengers", "Jurassic Park"]
+    ["strawberry","cherry","raspberry"],
+    ["new years eve","valentines","christmas"],
+    ["opening presents", " being in pageant", "visiting santa"],
+    ["comedian", "clown", "sports mascot"]
 ]    
 
-player2_questions = [""] 
-player2_answers = 
+player2_questions = ["Which Color Would You Be Surprised To See A Bride Wearing On Her Wedding Day? ", "Name a Night Of The Week When People Stay Up Later Than Usual? ", "Name A Kind Of Cookie People Love To Dunk In Milk? ","Name Something A Man Might Do At The Gym To Impress A Woman? "] 
+player2_answers = [
+  ["black", "red", "green"]
+  ["friday","saturday","sunday"]
+  ["chocolate chip",]
+  ["lift weights","flex","take off shirt"]
+]
 
-player3_questions = 
-player3_answers = 
-
-player4_questions = 
-player4_answers =
+player3_questions = ['Name The Most Used Piece Of Furniture In A House. ', 'Name a color you would see on a country flag ', 'Name a good gift for someone who is always late ', 'Name a vehicle with a siren ']
+player3_answers = [
+    ['couch', 'bed', 'chair']
+    ['red', 'blue', 'white']
+    ['watch','clock', 'calender']
+    ['police car', 'ambulance', 'firetruck']
+]
+ 
+ 
+ player4_questions = ["Name a part of the face", ]
+player4_answers = [
+    ['face', 'nose', 'mouth']
