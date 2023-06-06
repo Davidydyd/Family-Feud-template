e1f4fe04b2b08066783a8a2e914354444b0f7b4c
ThomasBiscan <134562752+ThomasBiscan@users.noreply.github.com>
2023-06-06 02:10:26 +0000

    added welcoming to game

--- list.py
+++ list.py
@@ -1,4 +1,4 @@
-player1_questions = ["If you see pink frosting on a cake, what flavor would you expect it to be? ", "Name A Specific Night Of The Year That You Hope To Be Kissed?", "What Might A Parent Videotape Their Child Doing Around The Christmas Holiday?", "	Name A Profession That’s Only Successful If People Are Laughing At Them?"]
+player1_questions = ["If you see pink frosting on a cake, what flavor would you expect it to be? ", "Name A Specific Night Of The Year That You Hope To Be Kissed?", "What Might A Parent Videotape Their Child Doing Around The Christmas Holiday?","Name A Profession That’s Only Successful If People Are Laughing At Them?"]
 player1_answers = [
     ["strawberry","cherry","raspberry"],
     ["new years eve","valentines","christmas"],
@@ -8,25 +8,25 @@ player1_answers = [
 
 player2_questions = ["Which Color Would You Be Surprised To See A Bride Wearing On Her Wedding Day? ", "Name a Night Of The Week When People Stay Up Later Than Usual? ", "Name A Kind Of Cookie People Love To Dunk In Milk? ","Name Something A Man Might Do At The Gym To Impress A Woman? "] 
 player2_answers = [
-  ["black", "red", "green"]
-  ["friday","saturday","sunday"]
-  ["chocolate chip",]
+  ["black", "red", "green"],
+  ["friday","saturday","sunday"],
+  ["chocolate chip"],
   ["lift weights","flex","take off shirt"]
 ]
 
 player3_questions = ['Name The Most Used Piece Of Furniture In A House. ', 'Name a color you would see on a country flag ', 'Name a good gift for someone who is always late ', 'Name a vehicle with a siren ']
 player3_answers = [
-    ['couch', 'bed', 'chair']
-    ['red', 'blue', 'white']
-    ['watch','clock', 'calender']
+    ['couch', 'bed', 'chair'],
+    ['red', 'blue', 'white'],
+    ['watch','clock', 'calender'],
     ['police car', 'ambulance', 'firetruck']
 ]
  
  
 player4_questions = ["Name a part of the face ", 'Name a food with a bitter taste ','Name something with a buckle ', 'Name a domestic pet ']
 player4_answers = [
-    ['face', 'nose', 'mouth']
-    ['lemon', 'orange', 'lime']
-    ['seatbelt', 'handbag', 'shoe']
+    ['face', 'nose', 'mouth'],
+    ['lemon', 'orange', 'lime'],
+    ['seatbelt', 'handbag', 'shoe'],
     ['dog', 'cat', 'hamster']
-[
+]
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-from list import*
+from list import *
 # Questions and answers                 
 
 
@@ -9,26 +9,42 @@ def populate_list():
 
 def play_game():
     # Select a random question
-    question_index = random.randint(0, len(questions) - 1)
-    question = questions[question_index]
-    possible_answers = answers[question_index]
-
-    print("Question:", question)
+    question_index = random.randint(0, 3)
+    question = player1_questions[question_index]
+    possible_answers = player1_answers[question_index]
+    print("\nQuestion:", question)
 
     # Shuffle the answers
     random.shuffle(possible_answers)
 
     # Get user's choice
-    user_choice = input("Your answer:")
+    user_choice = input("Your answer: ")
+    if user_choice.lower == possible_answers:
+      print("\nYou got it right!")
+    else:
+      print("\nYou got it wrong. The possible answers were:", possible_answers)
   
     # Check if the answer is correct
    
 
+
+
+
+
+#main part
+
+print("Welcome to our Family Feud Game. You will get asked 4 questions. The answers are from data we collected from a hundred people. Every correctly answered question will be one point. You can play this game in total as four players. Please answer all the question in lower case.")
+
 # Main loop
 while True:
     play_game()
-    play_again = input("Do you want to play again? (yes/no): ")
-    if play_again.lower() != "yes":
-        break
+    #play_again = input("Do you want to play again? (yes/no): ")
+    #if play_again.lower() != "yes":
+        #break
 
-print("Thank you for playing!")
+
+  
+
+
+
+#print("Thank you for playing!")
