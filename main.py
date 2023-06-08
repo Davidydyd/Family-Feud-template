17f62311dfca169bdf36edf4e33679f4c30ea5e4
Davidydyd <134562775+Davidydyd@users.noreply.github.com>
2023-06-08 00:32:30 +0000

    set the score after each player to 0

--- main.py
+++ main.py
@@ -8,33 +8,32 @@ def populate_list():
   pass
 
 
-def play_game():
+def play_game(questions, answers):
   global score
-  # Select a random question
-  length_of_list = len(player1_questions)-1
-  question_index = random.randint(0, length_of_list)
-  print("Question index chosen:",question_index)
-  length_of_list_answers = len(player1_answers)
-  question = player1_questions[question_index]
-  possible_answers = player1_answers[question_index]
-  print("\nQuestion:", question)
-
-  # Shuffle the answers
-  random.shuffle(possible_answers)
-
-  # Get user's choice
-  user_choice = input("Your answer: ")
-  if user_choice.lower() in possible_answers:
-    print("\nYou got it right!")
-    score +=1
-    print(f"Your score is {score}")
-    print(player1_questions)
-    print(player1_answers)
-    player1_questions.remove(player1_questions[question_index])
-    player1_answers.remove(player1_answers[question_index])
-  else:
-    print(f"\nYou got it wrong. You guessed {user_choice}. The possible answers were:", possible_answers)
-
+  for i in range(4):  
+    # Select a random question
+    length_of_list = len(questions)-1
+    question_index = random.randint(0, length_of_list)
+    length_of_list_answers = len(answers)
+    question = questions[question_index]
+    possible_answers = answers[question_index]
+    print("\nQuestion:", question)
+  
+    # Shuffle the answers
+    random.shuffle(possible_answers)
+  
+    # Get user's choice
+    user_choice = input("Your answer: ")
+    if user_choice.lower() in possible_answers:
+      print("\nYou got it right!")
+      score +=1
+      print(f"Your score is {score}")
+      questions.remove(questions[question_index])
+      answers.remove(answers[question_index])
+    else:
+      print(f"\nYou got it wrong. You guessed {user_choice}. The possible answers were:", possible_answers)
+      print(f"\nYour score is {score}")
+  
   
 
 
@@ -44,17 +43,34 @@ def play_game():
 
 #main part
 
-print("Welcome to our Family Feud Game. You will get asked 4 questions. The answers are from data we collected from a hundred people. Every correctly answered question will be one point. You can play this game in total as four players. Please answer all the question in lower case.")
-
+print("Welcome to our Family Feud Game. You will get asked 4 questions. The answers are from data we collected from a hundred people. Every correctly answered question will be one point. You can play this game in total as four players.")
 
 while True:
-    play_game()
-    
-  
-
-
-play_again = input("\nDo you want to play again? (yes/no): ")
-if play_again.lower() != "yes":
-    play_game()
-else:
-  print("Thank you for playing!")
+  play_game(player1_questions, player1_answers)
+  print(f"\nYou finished with a score of {score}")
+  score = 0
+  play_again = input("\nDo you want the next player to play? (yes/no): ")
+  if play_again.lower() == "yes":
+    play_game(player2_questions, player2_answers)
+    print(f"\nYou finished with a score of {score}")
+    score = 0
+    play_again = input("\nDo you want the next player to play? (yes/no): ")
+  else:
+    print("Thank you for playing!")
+    break
+    if play_again.lower() == "yes":
+      play_game(player3_questions, player3_answers)
+      print(f"\nYou finished with a score of {score}")
+      score = 0
+      play_again = input("\nDo you want the next player to play? (yes/no): ")
+    else:
+      print("Thank you for playing!")
+      break
+      if play_again.lower() == "yes":
+        play_game(player4_questions, player4_answers)
+        print(f"\nYou finished with a score of {score}")
+        score = 0
+        break
+      else:
+        print("Thank you for playing!")
+      break
